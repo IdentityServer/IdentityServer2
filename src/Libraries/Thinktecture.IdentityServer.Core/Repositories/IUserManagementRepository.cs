ae164b8fe60ac3438b561a9653f36439f1e907e3
Dominick Baier <dbaier@leastprivilege.com>
2012-09-12 12:55:47 +0200

    Added IUserManagement prototype

---
+++ src/Libraries/Thinktecture.IdentityServer.Core/Repositories/IUserManagementRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityServer.Repositories
+{
+    // prototype
+    public interface IUserManagementRepository
+    {
+        void CreateUser(string userName, string password, string displayName, string email);
+        void DeleteUser(string userName);
+        void SetRolesForUser(string userName, IEnumerable<string> roles);
+        IEnumerable<string> GetRolesForUser(string userName);
+
+        void CreateRole(string roleName);
+        void DeleteRole(string roleName);
+    }
+}
