87868992b0bba185d7f2892ba07d3c1032b0576f
Dominick Baier <dbaier@leastprivilege.com>
2012-09-13 20:42:09 +0200

    Added AdfsBridgeProtocol config

---
+++ src/Libraries/Thinktecture.IdentityServer.Core/Models/Configuration/AdfsBridgeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityServer.Models.Configuration
+{
+    public class AdfsBridgeConfiguration : ProtocolConfiguration
+    {
+        public string MixedUserNameEndpointAddress { get; set; }
+        public string IssuerThumbprint { get; set; }
+    }
+}
