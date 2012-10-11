a9b7bc17f78348999c0bd9d881ac178c209e0749
Dominick Baier <dbaier@leastprivilege.com>
2012-10-11 18:53:58 +0200

    Updated DiagConfig annotation

--- src/Libraries/Thinktecture.IdentityServer.Core/Models/Configuration/DiagnosticsConfiguration.cs
+++ src/Libraries/Thinktecture.IdentityServer.Core/Models/Configuration/DiagnosticsConfiguration.cs
@@ -4,11 +4,13 @@
  */
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Thinktecture.IdentityServer.Models.Configuration
 {
     public class DiagnosticsConfiguration
     {
+        [Display(Name = "Enable tracing of federation message", Description = "Enable tracing of federation message (may contain confidential data).")]
         public Boolean EnableFederationMessageTracing { get; set; }
     }
 }
