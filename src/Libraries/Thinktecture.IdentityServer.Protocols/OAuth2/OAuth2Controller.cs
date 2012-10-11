34dd4cbb0216d8a3559c36e37d12d3df7f7b1081
Dominick Baier <dbaier@leastprivilege.com>
2012-10-11 19:28:47 +0200

    Added OAuth Constants

--- src/Libraries/Thinktecture.IdentityServer.Protocols/OAuth2/OAuth2Controller.cs
+++ src/Libraries/Thinktecture.IdentityServer.Protocols/OAuth2/OAuth2Controller.cs
@@ -52,7 +52,7 @@ namespace Thinktecture.IdentityServer.Protocols.OAuth2
             }
 
             // check grant type
-            if (string.Equals(tokenRequest.Grant_Type, "password", System.StringComparison.Ordinal))
+            if (string.Equals(tokenRequest.Grant_Type, OAuthConstants.GrantTypes.Password, System.StringComparison.Ordinal))
             {
                 return ProcessResourceOwnerCredentialRequest(tokenRequest.UserName, tokenRequest.Password, appliesTo, tokenType);
             }
---
+++ src/Libraries/Thinktecture.IdentityServer.Protocols/Shared/OAuthConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityServer.Protocols
+{
+    public static class OAuthConstants
+    {
+        public static class GrantTypes
+        {
+            public const string Password = "password";
+        }
+    }
+}
