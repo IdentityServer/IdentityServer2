90e8c5c66141aea3cd818a12902f65844359927b
Dominick Baier <dbaier@leastprivilege.com>
2012-10-12 16:30:51 +0200

    Updated client model

--- src/Libraries/Thinktecture.IdentityServer.Core/Models/Client.cs
+++ src/Libraries/Thinktecture.IdentityServer.Core/Models/Client.cs
@@ -9,7 +9,7 @@ namespace Thinktecture.IdentityServer.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ReturnUrl { get; set; }
+        public string RedirectUri { get; set; }
 
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
