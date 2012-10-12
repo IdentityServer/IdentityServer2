591a52033482d73dc4b3d1a3c8b75046604eb7aa
Dominick Baier <dbaier@leastprivilege.com>
2012-10-12 16:19:06 +0200

    Added model for Client

--- src/Libraries/Thinktecture.IdentityServer.Core/Models/Client.cs
+++ src/Libraries/Thinktecture.IdentityServer.Core/Models/Client.cs
@@ -3,25 +3,15 @@
  * see license.txt
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Thinktecture.IdentityServer.Models
 {
-    // post RTM
-    //public class Client
-    //{
-    //    public string Name { get; set; }
-    //    public string ReturnUrl { get; set; }
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ReturnUrl { get; set; }
 
-    //    public string ClientId { get; set; }
-    //    public string ClientSecret { get; set; }
-
-    //    public string ExtraData1 { get; set; }
-    //    public string ExtraData2 { get; set; }
-    //    public string ExtraData3 { get; set; }
-    //}
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
 }
