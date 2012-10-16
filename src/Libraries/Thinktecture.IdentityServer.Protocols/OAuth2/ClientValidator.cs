1391c40cfbc75b3eb873b25d635e83f210368c26
Dominick Baier <dbaier@leastprivilege.com>
2012-10-16 19:47:16 +0200

    Added ClientValidator

---
+++ src/Libraries/Thinktecture.IdentityServer.Protocols/OAuth2/ClientValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Repositories;
+
+namespace Thinktecture.IdentityServer.Protocols.OAuth2
+{
+    public class ClientValidator
+    {
+        [Import]
+        public IClientsRepository Clients { get; set; }
+
+        public ClientValidator()
+        {
+            Container.Current.SatisfyImportsOnce(this);
+        }
+
+        public ClientValidator(IClientsRepository clients)
+        {
+            Clients = clients;
+        }
+
+        public bool ValidateClient(string clientId, string clientSecret)
+        {
+            return Clients.ValidateClient(clientId, clientSecret);
+        }
+    }
+}
