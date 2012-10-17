bc29f8db62fac90f645de8d3d70cae547df6befe
Dominick Baier <dbaier@leastprivilege.com>
2012-10-17 08:26:52 +0200

    Updated OAuth2 configuration model

--- src/Libraries/Thinktecture.IdentityServer.Core/Models/Configuration/OAuth2Configuration.cs
+++ src/Libraries/Thinktecture.IdentityServer.Core/Models/Configuration/OAuth2Configuration.cs
@@ -3,8 +3,19 @@
  * see license.txt
  */
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Thinktecture.IdentityServer.Models.Configuration
 {
     public class OAuth2Configuration : ProtocolConfiguration
-    { }
+    {
+        [Display(Name = "Enable 'Resource Owner Password Credential' Flow", Description = "This flow is for trusted applications only. Users enter their credentials directly into the client.")]
+        public bool EnableResourceOwnerFlow { get; set; }
+
+        [Display(Name = "Enable 'Implicit' Flow", Description = "This flow is for native apps and client web applications.")]
+        public bool EnableImplicitFlow { get; set; }
+
+        [Display(Name = "Require Client Authentication", Description = "Specifies whether client have to authenticate to use the token endpoint.")]
+        public bool RequireClientAuthentication { get; set; }
+    }
 }
