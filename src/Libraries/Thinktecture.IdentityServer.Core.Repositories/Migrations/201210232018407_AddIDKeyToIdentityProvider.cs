449b2cb82b2a53e5ae99f436250dda29ac41edae
Brock Allen <brockallen@gmail.com>
2012-10-23 16:21:32 -0400

    add migration for identity provider ID key

---
+++ src/Libraries/Thinktecture.IdentityServer.Core.Repositories/Migrations/201210232018407_AddIDKeyToIdentityProvider.cs
@@ -0,0 +1,22 @@
+namespace Thinktecture.IdentityServer.Core.Repositories.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIDKeyToIdentityProvider : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.IdentityProvider", "ID", c => c.Int(nullable: false, identity: true));
+            DropPrimaryKey("dbo.IdentityProvider", new[] { "Name" });
+            AddPrimaryKey("dbo.IdentityProvider", "ID");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.IdentityProvider", new[] { "ID" });
+            AddPrimaryKey("dbo.IdentityProvider", "Name");
+            DropColumn("dbo.IdentityProvider", "ID");
+        }
+    }
+}
