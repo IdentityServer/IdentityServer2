34c7bf7a247c8f39836661baa0ece7fd8ece30d5
Dominick Baier <dbaier@leastprivilege.com>
2012-11-07 20:27:45 +0100

    Added build script

---
+++ build.cmd
@@ -0,0 +1,30 @@
+@echo off
+pushd %~dp0
+
+if exist bin goto build
+mkdir bin
+
+:Build
+if "%1" == "" goto BuildDefaults
+
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Thinktecture.IdentityServer.sln /m /nr:false /t:%* /p:Platform="Any CPU" /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+if %ERRORLEVEL% neq 0 goto BuildFail
+goto BuildSuccess
+
+:BuildDefaults
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Thinktecture.IdentityServer.sln /m /nr:false /p:Platform="Any CPU" /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+if %ERRORLEVEL% neq 0 goto BuildFail
+goto BuildSuccess
+
+:BuildFail
+echo.
+echo *** BUILD FAILED ***
+goto End
+
+:BuildSuccess
+echo.
+echo **** BUILD SUCCESSFUL ***
+goto end
+
+:End
+popd
