0acfaf44e1ab981a4252ab85f566aa767dca1dd6
Dominick Baier <dbaier@leastprivilege.com>
2013-02-05 18:46:46 +0100

    Updated ADFS config

--- src/Libraries/Thinktecture.IdentityServer.Core/Models/Configuration/AdfsIntegrationConfiguration.cs
+++ src/Libraries/Thinktecture.IdentityServer.Core/Models/Configuration/AdfsIntegrationConfiguration.cs
@@ -10,14 +10,31 @@ namespace Thinktecture.IdentityServer.Models.Configuration
 {
     public class AdfsIntegrationConfiguration : ProtocolConfiguration
     {
-        [Display(Name = "ADFS WS-Trust Endpoint", Description = "Address of the UserNameMixed WS-Trust endpoint on ADFS")]
-        public Uri UserNameMixedModeEndpoint { get; set; }
+        // OAuth2 resource owner flow to ADFS feature
+
+        [Display(Name = "Enable user authentication", Description = "Enables the OAuth2 resource owner flow to ADFS feature")]      
+        public bool UserNameEnabled { get; set; }
+
+        [Display(Name = "ADFS UserName Endpoint", Description = "Address of the UserNameMixed WS-Trust endpoint on ADFS for user authentication")]
+        public Uri UserNameEndpoint { get; set; }
+
+        [Display(Name = "Create new token", Description = "If enabled, a new JWT token will be created, if not the original ADFS SAML token will be passed through")]
+        public bool CreateNewToken { get; set; }
+
         
+        // OAuth2 assertion flow to ADFS feature
+
+        [Display(Name = "Enable delegation", Description = "Enables the OAuth2 SAML assertion flow to ADFS delegation feature")]
+        public bool DelegationEnabled { get; set; }
+
+        [Display(Name = "ADFS Delegation Endpoint", Description = "Address of the WindowsMixed WS-Trust endpoint on ADFS for delegation")]
+        public Uri DelegationEndpoint { get; set; }
+
+
+        // Shared config options
+
         [Display(Name = "ADFS Signing Certificate Thumbprint", Description = "Thumbprint of the ADFS signing certificate. Only needed when creating new tokens")]
         public string IssuerSigningThumbprint { get; set; }
-
-        [Display(Name = "Create new Token", Description = "If enabled, a new JWT token will be created, if not the original ADFS SAML token will be passed through")]
-        public bool CreateNewToken { get; set; }
 
         [Display(Name = "Symmetric signing key", Description = "The symmetric singing key for the newly created token, empty if you want to use the default signing key")]
         public string SymmetricSigningKey { get; set; }
