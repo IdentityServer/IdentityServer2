8434d8cf9f91bec6413fad7cac2b7ed0fbdba895
Brock Allen <brockallen@gmail.com>
2013-03-21 17:46:31 -0400

    added migrations for new adfs integration schema

---
+++ src/Libraries/Thinktecture.IdentityServer.Core.Repositories/Migrations.SqlServer/201303212116088_AdfsIntegration.cs
@@ -0,0 +1,36 @@
+namespace Thinktecture.IdentityServer.Core.Repositories.Migrations.SqlServer
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdfsIntegration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AdfsIntegrationConfiguration",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Enabled = c.Boolean(nullable: false),
+                        UsernameAuthenticationEnabled = c.Boolean(nullable: false),
+                        SamlAuthenticationEnabled = c.Boolean(nullable: false),
+                        JwtAuthenticationEnabled = c.Boolean(nullable: false),
+                        PassThruAuthenticationToken = c.Boolean(nullable: false),
+                        AuthenticationTokenLifetime = c.Int(nullable: false),
+                        UserNameAuthenticationEndpoint = c.String(),
+                        FederationEndpoint = c.String(),
+                        IssuerUri = c.String(),
+                        IssuerThumbprint = c.String(),
+                        EncryptionCertificate = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.AdfsIntegrationConfiguration");
+        }
+    }
+}
