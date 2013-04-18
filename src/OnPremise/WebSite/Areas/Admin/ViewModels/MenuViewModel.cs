25254d6f7e2a367622188b259ec67fd8fd143688
Brock Allen <brockallen@gmail.com>
2013-04-18 18:16:58 -0400

    hide client certs and delegation menu items if not enabled

--- src/OnPremise/WebSite/Areas/Admin/ViewModels/MenuViewModel.cs
+++ src/OnPremise/WebSite/Areas/Admin/ViewModels/MenuViewModel.cs
@@ -10,9 +10,13 @@ namespace Thinktecture.IdentityServer.Web.Areas.Admin.ViewModels
             this.ShowOAuthTokens = 
                 configuration.OAuth2.Enabled && 
                 (configuration.OAuth2.EnableCodeFlow || configuration.OAuth2.EnableResourceOwnerFlow);
+            this.ShowClientCerts = configuration.Global.EnableClientCertificateAuthentication;
+            this.ShowIdentityDelegation = configuration.WSTrust.EnableDelegation;
         }
 
         public bool ShowOAuthClients { get; private set; }
         public bool ShowOAuthTokens { get; private set; }
+        public bool ShowClientCerts { get; private set; }
+        public bool ShowIdentityDelegation { get; private set; }
     }
 }
