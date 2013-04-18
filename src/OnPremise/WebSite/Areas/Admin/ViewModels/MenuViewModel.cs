ea0b5cf2740a4bb5e61961f8af8559a2a4ba6262
Brock Allen <brockallen@gmail.com>
2013-04-18 18:30:43 -0400

    hide menu for identity providers if federation is disabled

--- src/OnPremise/WebSite/Areas/Admin/ViewModels/MenuViewModel.cs
+++ src/OnPremise/WebSite/Areas/Admin/ViewModels/MenuViewModel.cs
@@ -6,14 +6,16 @@ namespace Thinktecture.IdentityServer.Web.Areas.Admin.ViewModels
     {
         public MenuViewModel(IConfigurationRepository configuration)
         {
+            this.ShowIdentityProviders = configuration.WSFederation.Enabled && configuration.WSFederation.EnableFederation;
             this.ShowOAuthClients = configuration.OAuth2.Enabled;
             this.ShowOAuthTokens = 
                 configuration.OAuth2.Enabled && 
                 (configuration.OAuth2.EnableCodeFlow || configuration.OAuth2.EnableResourceOwnerFlow);
             this.ShowClientCerts = configuration.Global.EnableClientCertificateAuthentication;
-            this.ShowIdentityDelegation = configuration.WSTrust.EnableDelegation;
+            this.ShowIdentityDelegation = configuration.WSTrust.Enabled && configuration.WSTrust.EnableDelegation;
         }
 
+        public bool ShowIdentityProviders { get; private set; }
         public bool ShowOAuthClients { get; private set; }
         public bool ShowOAuthTokens { get; private set; }
         public bool ShowClientCerts { get; private set; }
