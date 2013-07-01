bb6ec692e9fb067dc9c8fe72a52db79ae71687f4
Dominick Baier <dbaier@leastprivilege.com>
2013-07-01 12:06:44 +0200

    Added UserInfo prototype

---
+++ src/Libraries/Thinktecture.IdentityServer.Protocols/OpenIdConnect/UserInfoController.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Protocols.WSTrust;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using Thinktecture.IdentityModel.Constants;
+
+namespace Thinktecture.IdentityServer.Protocols.OpenIdConnect
+{
+    [Authorize]
+    public class UserInfoController : ApiController
+    {
+        HttpResponseMessage Get()
+        {
+            var requestClaims = new RequestClaimCollection();
+
+            var scopes = ClaimsPrincipal.Current.FindAll(OAuth2Constants.Scope);
+            foreach (var scope in scopes)
+            {
+                if (OidcConstants.Mappings.ContainsKey(scope.Value))
+                {
+                    foreach (var oidcClaim in OidcConstants.Mappings[scope.Value])
+                    {
+                        requestClaims.Add(new RequestClaim(oidcClaim));
+                    }
+                }
+                else
+                {
+                    Request.CreateErrorResponse(HttpStatusCode.BadRequest, "invalid scope");
+                }
+            }
+
+            // populate RequestDetails
+            // call ClaimsRepository
+
+            throw new NotImplementedException();
+        }
+    }
+}
