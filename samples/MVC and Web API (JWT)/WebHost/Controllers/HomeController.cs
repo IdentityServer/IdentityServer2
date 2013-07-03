3e2390d9d12e6a183b145f2901ab5fd48d900ba6
Dominick Baier <dbaier@leastprivilege.com>
2013-07-03 20:18:34 +0200

    Added a WS-Federation MVC sample using JWT

---
+++ samples/MVC and Web API (JWT)/WebHost/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace WebHost.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
