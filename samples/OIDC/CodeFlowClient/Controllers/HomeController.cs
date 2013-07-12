a8cc37fe91e5f5f4ccef7780dce9f0a807cdce2d
Dominick Baier <dbaier@leastprivilege.com>
2013-07-12 21:56:05 +0200

    Prototype of OIDC client module

--- samples/OIDC/CodeFlowClient/Controllers/HomeController.cs
+++ samples/OIDC/CodeFlowClient/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace CodeFlowClient.Controllers
             return View();
         }
 
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
@@ -22,6 +23,7 @@ namespace CodeFlowClient.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
--- samples/OIDC/Thinktecture.IdentityModel.Oidc/OpenIdConnectAuthenticationModule.cs
+++ samples/OIDC/Thinktecture.IdentityModel.Oidc/OpenIdConnectAuthenticationModule.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IdentityModel.Services;
+using System.IdentityModel.Tokens;
+using System.Net.Http;
+using System.Security.Claims;
 using System.Web;
+using System.Web.Configuration;
+using Thinktecture.IdentityModel.Web;
 
 namespace Thinktecture.IdentityModel.Oidc
 {
@@ -18,19 +22,101 @@ namespace Thinktecture.IdentityModel.Oidc
         void OnEndRequest(object sender, EventArgs e)
         {
             var context = HttpContext.Current;
-
-            // check for 401 - turn to 302 to oidc provider
-
-            if (context.Response.StatusCode == 401 && context.User.Identity.IsAuthenticated)
+            
+            if (context.Response.StatusCode == 401 && 
+                !context.User.Identity.IsAuthenticated &&
+                !context.Response.SuppressFormsAuthenticationRedirect)
             {
-                context.Response.Redirect("https://idsrv.local/issue/oidc/authorize?client_id=oidccode");
+                var state = Guid.NewGuid().ToString("N");
+                var returnUrl = HttpUtility.UrlEncode(context.Request.RawUrl, context.Request.ContentEncoding);
+                state = state + "_" + returnUrl;
+                
+                var authorizeUrl = WebConfigurationManager.AppSettings["oidc:authorizeUrl"];
+                var clientId = WebConfigurationManager.AppSettings["oidc:clientId"];
+                var scopes = "openid " + WebConfigurationManager.AppSettings["oidc:scopes"];
+                var redirectUri = "https://localhost:44309/" + "oidccallback";
+                
+                var queryString = string.Format("?client_id={0}&scope={1}&redirect_uri={2}&state={3}",
+                    clientId,
+                    scopes,
+                    redirectUri,
+                    state);
+
+                var cookie = new ProtectedCookie(ProtectionMode.MachineKey);
+                cookie.Write("oidcstate", state, DateTime.UtcNow.AddHours(1));
+                
+                context.Response.Redirect("https://idsrv.local/issue/oidc/authorize" + queryString);
             }
         }
 
         void OnAuthenticateRequestRequest(object sender, EventArgs e)
         {
-            // check for callback and do back channel communication
-            // establish session afterwards
+            var context = HttpContext.Current;
+
+            if (context.Request.Url.AbsoluteUri.Equals("https://localhost:44309/" + "oidccallback", StringComparison.OrdinalIgnoreCase))
+            {
+                var code = context.Request.QueryString["code"];
+                var state = context.Request.QueryString["state"];
+                var error = context.Request.QueryString["error"];
+
+                var tokenUrl = WebConfigurationManager.AppSettings["oidc:tokenUrl"];
+                var clientId = WebConfigurationManager.AppSettings["oidc:clientId"];
+                var clientSecret = WebConfigurationManager.AppSettings["oidc:clientSecret"];
+                
+                var cookie = new ProtectedCookie(ProtectionMode.MachineKey);
+                var storedState = cookie.Read("oidcstate");
+
+                if (storedState != state)
+                {
+                    throw new InvalidOperationException("state invalid.");
+                }
+
+                var returnUrl = state.Split('_')[1];
+
+                // do back channel communication
+                //  token endpoint
+                //  userinfo endpoint
+                var client = new HttpClient
+                {
+                    BaseAddress = new Uri(tokenUrl)
+                };
+                
+                client.SetBasicAuthentication(clientId, clientSecret);
+
+                var parameter = new Dictionary<string, string>
+                {
+                    { "grant_type", "authorization_code" },
+                    { "code", code },
+                    { "redirect_uri", "https://localhost:44309/" + "oidccallback" }
+                };
+                
+                var response = client.PostAsync("", new FormUrlEncodedContent(parameter)).Result;
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    throw new InvalidOperationException("error calling token endpoint");
+                }
+
+                var oidcResponse = response.Content.ReadAsStringAsync().Result;
+                var oidcJson = JObject.Parse(oidcResponse);
+
+                var idToken = oidcJson["id_token"].ToString();
+                var principal = ValidateIdentityToken(idToken);
+
+                // establish session
+                var sessionToken = new SessionSecurityToken(principal);
+                FederatedAuthentication.SessionAuthenticationModule.WriteSessionTokenToCookie(sessionToken);
+
+                
+                // redirect local to return url
+                context.Response.Redirect(returnUrl);
+            }
+
+            
+        }
+
+        private ClaimsPrincipal ValidateIdentityToken(string idToken)
+        {
+            throw new NotImplementedException();
         }
 
         public void Dispose()
