b2ababa81f343c44c1d6e3a5127af7ce3b5bbcad
Dominick Baier <dbaier@leastprivilege.com>
2013-07-26 17:18:07 +0200

    Added OWIN boilerplate code

---
+++ samples/OIDC/Thinktecture.IdentityModel.Oidc/OWIN/OpenIdConnectAuthenticationHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityModel.Oidc.OWIN
+{
+    class OpenIdConnectAuthenticationHandler
+    {
+    }
+}
